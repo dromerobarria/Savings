dc9ee7a232e9dbb574d6d5d5f831d315f4c855ca
Daniel Romero <dromero@Daniels-MacBook-Pro-756.local>
2024-07-26 16:36:26 -0400

    chore

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -6,5 +6,11 @@
 #  Created by Daniel Romero on 26-07-24.
 #  
 
-brew install swiftlint
-swiftlint $CI_WORKSPACE
+install_or_update_swiftlint_if_needed() {
+    echo "Install SwiftLint"
+    brew install swiftlint
+    swiftlint $CI_WORKSPACE
+    return 0
+}
+
+install_or_update_swiftlint_if_needed
